43be6207e6e1011291ec3fff08849f21742f6d7e
Maria Fedorova <maria.i.fedorova@yandex.ru>
2024-01-27 00:37:49 +0300

    str_func.py was changed by adding capital_letter function

--- str_func.py
+++ str_func.py
@@ -1,6 +1,14 @@
-"""Функция перевоода в заглавные буквы"""
+"""Добавила докстринг"""
 
 
 def capital_letters(string):
     capitals = string.upper()
     return capitals
+
+
+"""Функция делает заглавными только первые буквы слова"""
+
+
+def capital_letter(string):
+    capital = string.title()
+    return capital
